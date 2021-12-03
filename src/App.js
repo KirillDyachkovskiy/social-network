4da3d3fc6558cdd78ef99bef15dbdabfe13c2949
KirillDyachkovskiy <mohnachevskii.K@edu.rea.ru>
2021-12-04 00:37:40 +0300

    begin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="main-grid">
+          <header className="main-grid__header header">
+            <img src="./logo.png" alt="kirillDN" className="header__log" />
+          </header>
+          <nav className="main-grid__navbar navbar">
+            <div className="navbar__profile">
+              Profile
+            </div>
+            <div className="navbar__messages">
+              Messages
+            </div>
+            <div className="navbar__news">
+              News
+            </div>
+            <div className="navbar__music">
+              Music
+            </div>
+            <div className="navbar__setting">
+              Setting
+            </div>
+          </nav>
+          <main className="main-grid__content content">
+            <div className="content__main">
+              Main block
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
