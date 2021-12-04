c5dff07a23cd7643d9b610e2231a1c4da483e3d8
KirillDyachkovskiy <mohnachevskii.K@edu.rea.ru>
2021-12-04 22:16:36 +0300

    step 1

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import './App.css';
 import React from "react";
-import "./App.css";
+import "./css/style.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        <div className="main-grid">
-          <header className="main-grid__header header">
-            <img src="./logo.png" alt="kirillDN" className="header__log" />
-          </header>
-          <nav className="main-grid__navbar navbar">
-            <div className="navbar__profile">
-              Profile
-            </div>
-            <div className="navbar__messages">
-              Messages
-            </div>
-            <div className="navbar__news">
-              News
-            </div>
-            <div className="navbar__music">
-              Music
-            </div>
-            <div className="navbar__setting">
-              Setting
-            </div>
-          </nav>
-          <main className="main-grid__content content">
-            <div className="content__main">
-              Main block
-            </div>
-          </main>
-        </div>
+
       </div>
     );
   }
