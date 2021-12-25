228bde872e82e490c9a28e3d05d7ae10df9d3945
KirillDyachkovskiy <mohnachevskii.K@edu.rea.ru>
2021-12-25 14:48:53 +0300

    learned 52 lessons

--- src/components/pages/Friends/Content/Users/UsersStateless.jsx
+++ src/components/pages/Friends/Content/Users/UsersStateless.jsx
@@ -1,20 +1,27 @@
 import * as axios from 'axios';
+import React from 'react';
 import { User } from './User';
 import c from './Users.module.scss';
 
 export const UsersStateless = ({ users, setUsersList, onButtonClick }) => {
-    if (users.length === 0) {
-        axios
-            .get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(({ data }) => {
-                setUsersList(data.items);
-            });
+    const getUsers = () => {
+        if (users.length === 0) {
+            axios
+                .get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(({ data }) => {
+                    setUsersList(data.items);
+                });
+        }
+
     }
     const list = users.map(u => <User key={u.id} user={u} onButtonClick={onButtonClick} />);
 
     return (
-        <div className={c.users}>
-            {list}
-        </div>
+        <>
+            <button onClick={getUsers}>Найти пользователей</button>
+            <div className={c.users}>
+                {list}
+            </div>
+        </>
     )
 }
