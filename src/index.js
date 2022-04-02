13bed5ab73859364eea1fe3964b7fae22b0ad03e
Kirill <kirilldyachkovskiy@gmail.com>
2022-04-02 20:27:45 +0300

    update react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 import {store} from './services/redux';
-import './App.css';
 import {App} from "./App";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App/>
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-document.getElementById('root')
-);
+  </React.StrictMode>
+)
